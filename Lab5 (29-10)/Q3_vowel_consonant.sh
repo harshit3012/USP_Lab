a7feeece40eff9f394f73d0c67190912aa82538a
harshit3012 <harshit.hir2011@gmail.com>
2020-11-05 14:41:12 +0530

    Added extra programs on 05-11

---
+++ Lab5 (29-10)/Q3_vowel_consonant.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+# Check whether entered character is consonant or vowel
+
+echo "Enter a character: \c"
+read ch
+yes="It is a vowel."
+case $ch in
+	a) echo "$yes";;
+	A) echo "$yes";;
+	e) echo "$yes";;
+	E) echo "$yes";;
+	i) echo "$yes";;
+	I) echo "$yes";;
+	o) echo "$yes";;
+	O) echo "$yes";;
+	u) echo "$yes";;
+	U) echo "$yes";;
+	*) echo "It is a consonant.";;
+esac
